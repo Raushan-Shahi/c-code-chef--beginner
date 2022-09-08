0cac062b64c040e1ec779ef99d792c73561d136e
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-09-08 20:21:24 +0530

    day 7

---
+++ boot22.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+    
+int main(){
+    int t;
+    cin >>t;
+    int i,a[t],b[t],c[t];
+    for(i=0;i<t;i++)
+    {
+        cin >>a[i]>>b[i]>>c[i];
+    }
+    for(i=0;i<t;i++)
+    {
+        if((c[i]>=a[i])&&(c[i]<b[i]))
+        {
+            cout<< "yes"<<endl;
+        }
+        else 
+        {
+            cout <<"no"<<endl;
+        }
+    }    
+return 0;
+}
