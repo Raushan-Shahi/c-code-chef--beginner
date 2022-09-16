d762094896ed14d6c78d14b89a2126fcaa74c6a0
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-09-16 22:03:30 +0530

    day 13 beg

---
+++ boot34.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+int chkPair(int A[], int size, int x)
+{
+
+    for (int i = 0; i < (size - 1); i++)
+    {
+
+        for (int j = (i + 1); j < size; j++)
+        {
+
+            if (A[i] + A[j] == x)
+            {
+
+                printf("Pair with a given sum %d is (%d, %d)\n", x, A[i], A[j]);
+
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main(void)
+{
+
+    int A[] = {1,4,6,7,3,2,9};
+    int x = 13;
+
+    int size = sizeof(A) / sizeof(A[0]);
+
+    if (chkPair(A, size, x))
+        printf("Valid pair exists\n");
+    return 0;
+}
