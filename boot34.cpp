04be2ed1b7877d217488b8270438f1b9fef113a6
Raushan-Shahi <raushanshahi2001@gmail.com>
2022-09-21 21:53:09 +0530

    day 20 beg

--- boot34.cpp
+++ boot34.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
-    
-int main(){
-    int a,b;
-    a=90;
-    b=80;
-    int c = a+b;
-    cout <<c;
-return 0;
+
+int main()
+{
+    int a, b;
+    a = 90;
+    b = 80;
+    int c = a + b;
+    cout << c << endl;
+    return 0;
 }
